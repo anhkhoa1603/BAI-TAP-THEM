63ca7a473cadbdbd1487698704715468a42efbab
anhkhoa1603 <anhkhoa162003@gmail.com>
2021-10-22 16:06:35 +0700

    [+] NOP-BAI

---
+++ BAI-1/baiLam.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+
+//Hàm để nhập số phần tử
+void nhap(int m[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("nhap vao so thu %d:", i + 1);
+        scanf("%d", &m[i]);
+    }
+}
+
+//Ước chung lớn nhất của 2 phần tử
+int UC(int x, int y)
+{
+    if (x > y)
+    {
+        for (int i = x; i >= 1; i--)
+        {
+            if (x % i == 0 && y % i == 0)
+            {
+                return i;
+            }
+        }
+    }
+    else
+    {
+        for (int i = y; i >= 1; i--)
+        {
+            if (x % i == 0 && y % i == 0)
+            {
+                return i;
+            }
+        }
+    }
+}
+
+//Ước chung lớn nhất của mảng số
+int UCLN(int m[], int n)
+{
+    int x = m[0];
+    for (int i = 1; i < n; i++)
+    {
+        x = UC(x, m[i]);
+    }
+    return x;
+}
+
+int BC( int x, int y)
+{
+    if (x<y)
+    {
+        for (int i = x; i <= x*y; i++)
+        {
+            if (i%x==0 && i%y==0)
+            {
+                return i;
+            }
+        }
+    }
+    else
+    {
+        for (int i = y; i <= x*y; i++)
+        {
+            if (i%x==0 && i%y==0)
+            {
+                return i;
+            }
+        }
+    }
+    return y;
+} 
+
+int BCNN(int m[],int n)
+{
+    int x = m[0];
+    for (int i = 1; i < n; i++)
+    {
+        x = BC(x, m[i]);
+    }
+    return x;
+}
+int main()
+{
+    int m[100];
+    int n;
+    printf("Nhap vao so phan tu:");
+    scanf("%d",&n);
+    nhap(m,n);
+
+    printf("Uoc chung lon nhat cua %d phan tu tren la %d\n",n ,UCLN(m,n));
+
+    printf("Boi chung nho nhat cua %d phan tu tren la %d\n",n, BCNN(m,n));
+
+    return 0;
+}
